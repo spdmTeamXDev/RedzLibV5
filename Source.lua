f24c16985f3591f18625df98be812bf4b5dc6cfd
AstroX <dreddthedeath@gmail.com>
2024-12-14 15:06:21 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -45,8 +45,20 @@ local redzlib = {
 			["Color Theme"] = Color3.fromRGB(150, 0, 255),
 			["Color Text"] = Color3.fromRGB(240, 240, 240),
 			["Color Dark Text"] = Color3.fromRGB(180, 180, 180)
-		}
-	},
+		}, 
+		DarkRed = {
+		["Color Hub 1"] = ColorSequence.new({
+        ColorSequenceKeypoint.new(0.00, Color3.fromRGB(28, 23, 25.5)),
+        ColorSequenceKeypoint.new(0.50, Color3.fromRGB(30.5, 30.5, 30.5)),
+        ColorSequenceKeypoint.new(1.00, Color3.fromRGB(28, 23, 25.5))
+    }),
+    ["Color Hub 2"] = Color3.fromRGB(28, 28, 28),
+    ["Color Stroke"] = Color3.fromRGB(38, 38, 38),
+    ["Color Theme"] = Color3.fromRGB(255, 0, 0),
+    ["Color Text"] = Color3.fromRGB(240, 240, 240),
+    ["Color Dark Text"] = Color3.fromRGB(180, 180, 180)
+     }  
+},
 	Info = {
 		Version = "1.1.0"
 	},
